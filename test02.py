f9fe60b20d8a744defb6eb382aa49a60654e24aa
MasterVVK <vvk@2140101.ru>
2024-05-29 10:50:54 +0500

    Create test02.py

---
+++ test02.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Одномерный массив из 10 нулей
+array1 = np.zeros(10)
+print(array1)
+
+# Одномерный массив из 10 нулей
+array2 = np.zeros([10])
+print(array2)
+
+# Двумерный массив размером 1x10 из нулей
+array3 = np.zeros([1, 10])
+print(array3)
